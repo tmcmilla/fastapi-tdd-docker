8d1d24e0097c9a83656615565c37247c1da73de2
Thomas McMillan <thomasc.mcmillan@gmail.com>
2022-04-04 19:57:23 -0700

    Fixed ping test

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -11,4 +11,4 @@ def test_ping(test_app):
 
     # Then
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
+    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
